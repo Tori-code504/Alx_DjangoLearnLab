46cf07b3d8d1b82e5c4384ffa2bbe207b462c2c2
Victoria <toriamwendeo11@gmail.com>
2025-07-20 22:46:18 +0300

    Add some modified file content

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -25,9 +25,9 @@ urlpatterns += [
 
 
 urlpatterns += [
-    path('books/add/', views.add_book_view, name='add_book'),
-    path('books/edit/<int:pk>/', views.edit_book_view, name='edit_book'),
-    path('books/delete/<int:pk>/', views.delete_book_view, name='delete_book'),
+    path('add_book/', views.add_book_view, name='add_book'),
+    path('edit_book/<int:pk>/', views.edit_book_view, name='edit_book'),
+    path('delete_book/<int:pk>/', views.delete_book_view, name='delete_book'),
 ]
 
 
