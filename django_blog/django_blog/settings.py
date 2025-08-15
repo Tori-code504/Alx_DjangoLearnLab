d2def42ce9ee30d72d8ad4206bff4dd7076738ce
Victoria <toriamwendeo11@gmail.com>
2025-08-15 15:19:05 +0300

    updating settings.py

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -77,6 +77,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        'USER': 'Nexus',
+        'PASSWORD': '1234', 'USER': 'anything',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
